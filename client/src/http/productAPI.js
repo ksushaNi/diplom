93fb08d81aadf8b4cca79c593cae27a2cead36a7
ksushaNi <night.dream5@yandex.ru>
2026-06-16 22:05:19 +0200

    bebebe

--- client/src/http/productAPI.js
+++ client/src/http/productAPI.js
@@ -1,9 +1,9 @@
 import { $authHost, $host } from ".";
 
-// export const createType = async (type) => {
-//     const {data} = await $authHost.post('api/type', type)
-//     return data
-// }
+export const createType = async (type) => {
+    const {data} = await $authHost.post('api/type', type)
+    return data
+}
 export const fetchTypes = async () => {
     const {data} = await $host.get('api/type')
     console.log('📦 Ответ сервера:', data)
