b4f435ffad8b53b52c8c0d73ad59970c503ebc2e
ksushaNi <night.dream5@yandex.ru>
2026-06-16 18:44:23 +0200

    Исправил порядок CORS

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpLoad({}))
+app.use('/api', router)
+
+app.use(errorHandler)
+
 const express = require('express')
 const sequelize = require('./db') //импорт объекта из файла db
 const models = require('./models/models')//импорт моделей из файла models
@@ -10,19 +19,19 @@ const fileUpLoad = require('express-fileupload')
 const path = require('path')
 
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpLoad({}))
-app.use('/api', router)
-app.use((req, res, next) => {
-    res.removeHeader('Content-Security-Policy')
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    next()
-})
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.static(path.resolve(__dirname, 'static')))
+// app.use(fileUpLoad({}))
+// app.use('/api', router)
+// app.use((req, res, next) => {
+//     res.removeHeader('Content-Security-Policy')
+//     res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     next()
+// })
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 const start = async () => {
     try {
