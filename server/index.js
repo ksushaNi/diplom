48540146ad57a2c6862e4ae68550620c2a1addb3
ksushaNi <night.dream5@yandex.ru>
2026-06-16 19:10:33 +0200

    Исправил порядок объявления app

--- server/index.js
+++ server/index.js
@@ -1,13 +1,4 @@
 require('dotenv').config()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpLoad({}))
-app.use('/api', router)
-
-app.use(errorHandler)
-
 const express = require('express')
 const sequelize = require('./db') //импорт объекта из файла db
 const models = require('./models/models')//импорт моделей из файла models
@@ -19,19 +10,18 @@ const fileUpLoad = require('express-fileupload')
 const path = require('path')
 
 const app = express()
-// app.use(cors())
-// app.use(express.json())
-// app.use(express.static(path.resolve(__dirname, 'static')))
-// app.use(fileUpLoad({}))
-// app.use('/api', router)
-// app.use((req, res, next) => {
-//     res.removeHeader('Content-Security-Policy')
-//     res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     next()
-// })
 
-// app.use(errorHandler)
+app.use(cors({
+    origin: 'https://ksushani-diplom-e7eb.twc1.net',
+    credentials: true
+}))
+app.options('*', cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpLoad({}))
+app.use('/api', router)
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
