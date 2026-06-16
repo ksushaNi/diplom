043fbf5289b7fd54f18917503e5f8201514cb447
ksushaNi <night.dream5@yandex.ru>
2026-06-16 19:28:37 +0200

    Исправил :userId на :id в orderRoutes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewear/authMiddleware')
 const adminMiddleware = require('../middlewear/adminMiddleware')
 
 router.get('/', authMiddleware, adminMiddleware, orderController.getAllOrders)  // админ все заказы
-router.get('/user/:userId', authMiddleware, orderController.getUserOrders)       // заказы пользователя
+router.get('/user/:id', authMiddleware, orderController.getUserOrders)       // заказы пользователя
 router.put('/:id/status', authMiddleware, adminMiddleware, orderController.updateOrderStatus) // обновить статус
 router.post('/', authMiddleware, orderController.createOrder)
 
